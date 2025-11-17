6531627e1bfdd810eab15da0bae0e1ddbc883053
JJBBPP1 <147921148+JJBBPP1@users.noreply.github.com>
2025-11-17 13:22:28 +0100

    Add files via upload

---
+++ proyecto/js/script.js
@@ -0,0 +1,25 @@
+const display = document.getElementById('display');
+
+function appendValue(value) {
+    if(display.value === "0") {
+        display.value = value;
+    } else {
+        display.value += value;
+    }
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
